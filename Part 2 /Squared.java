21d99feaee6e6195790861c7fec8b0df319caa40
Lilacc777 <lindsayli390@gmail.com>
2023-09-16 21:46:32 -0400

    Deleted an unnecessary folder.

--- Part 2 /Squared.java
+++ Part 2 /Squared.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
 
+public class Squared {
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int integer = Integer.valueOf(scanner.nextLine());
+        int squared = integer * integer;
+
+        System.out.println(squared);
+   } 
+}
