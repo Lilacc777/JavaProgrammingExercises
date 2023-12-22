59c85366915d316e3b3729978cd0464d142ae644
Lilacc777 <lindsayli390@gmail.com>
2023-12-21 19:25:49 -0500

    edited 2 files

---
+++ Part3/Indexwasnotfound.java
@@ -0,0 +1,32 @@
+//Note: I don't have access to the original template for this exercise so I attempted to recreate something similar.
+
+package Part3;
+import java.util.Scanner;
+
+public class Indexwasnotfound {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 3;
+        numbers[2] = 5;
+        numbers[3] = 7;
+        numbers[4] = 9;
+
+        System.out.print("Search for? ");
+        Integer search = Integer.valueOf(scanner.nextLine());
+        
+        boolean trueOrFalse = false;
+        for(int i = 0; i < 5; i++){
+            if(numbers[i] == search) {
+                System.out.println(search + " is at index " + i + ".");
+                trueOrFalse = true;
+            } else if (trueOrFalse == false && i == 4){
+                System.out.println(search + " was not found.");
+                break;
+            } else {
+                continue;
+            }
+        }
+    }
+}
---
+++ Part3/Swap.java
@@ -0,0 +1,33 @@
+package Part3;
+
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int[] indices = new int[5];
+        indices[0] = 1;
+        indices[1] = 3;
+        indices[2] = 5;
+        indices[3] = 7;
+        indices[4] = 9;
+
+        for(int i = 0; i < 5; i++){
+            System.out.println(indices[i]);
+        }
+        
+        System.out.println("");
+        System.out.println("Give two indices to swap:");
+        int index1 = Integer.valueOf(scanner.nextLine());
+        int index2 = Integer.valueOf(scanner.nextLine());
+        
+        int helper = indices[index1];
+        indices[index1] = indices[index2];
+        indices[index2] = helper;
+        
+        System.out.println("");
+        for(int i = 0; i < 5; i++){
+            System.out.println(indices[i]);
+        }
+    }
+}
