8f3311d13baf379b86fc996f335b296c7ffb02df
Lilacc777 <lindsayli390@gmail.com>
2023-12-24 21:51:29 -0500

    edited files for exercises

--- Part4/NumbersFromaFile.java
+++ Part4/NumbersFromaFile.java
@@ -1,5 +1,37 @@
+//Note: The txt files "numbers-1.txt" and "numbers-2.txt" are used in this file.
 package Part4;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.nio.file.Paths;
+
 public class NumbersFromaFile {
-    
+    public static void main(String args[]){
+        Scanner scannerForFile = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.print("File? ");
+        String file = scannerForFile.nextLine();
+        System.out.print("Lower bound? ");
+        int lower = Integer.valueOf(scannerForFile.nextLine());
+        System.out.print("Upper bound? ");
+        int upper = Integer.valueOf(scannerForFile.nextLine());
+        int count = 0;
+
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+            while (scanner.hasNextLine()) {
+                int number = Integer.valueOf(scanner.nextLine());
+                if(number >= lower && number <= upper){
+                    numbers.add(number);
+                    count++;
+                }
+            }
+        
+        System.out.println("Numbers: " + count);
+        
+        scannerForFile.close();
+
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+    }
 }
